3d0f984c6a83f1cc17a8be6b382ee7515cd01bc0
StahlFerro <StahlFerro@users.noreply.github.com>
2020-10-15 12:03:03 +0700

    Set Producer's kafkaTemplate.send() method handling to be async

--- src/main/java/io/stahlferro/blumeproducer/services/Producer.java
+++ src/main/java/io/stahlferro/blumeproducer/services/Producer.java
@@ -3,7 +3,10 @@ package io.stahlferro.blumeproducer.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
 @Slf4j
@@ -13,6 +16,19 @@ public class Producer {
 
     public void sendMessage(String message) {
         log.info("#### -> Producing message -> " + message);
-        this.kafkaTemplate.send("users", message);
+        ListenableFuture<SendResult<String, String>> future = this.kafkaTemplate.send("users", message);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.info("Unable to send message=["
+                    + message + "] due to : " + ex.getMessage());
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+        });
     }
 }
