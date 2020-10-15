5031e9478daa9094d1131bada97052eae82e6a57
StahlFerro <StahlFerro@users.noreply.github.com>
2020-10-15 10:41:36 +0700

    First commit

---
+++ src/main/java/io/stahlferro/blumeproducer/BlumeProducerApplication.java
@@ -0,0 +1,13 @@
+package io.stahlferro.blumeproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlumeProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlumeProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/stahlferro/blumeproducer/BlumeProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stahlferro.blumeproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlumeProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
